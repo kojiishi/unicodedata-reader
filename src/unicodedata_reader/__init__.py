c383d101c83031cc97acca65a72ece69c204e3ad
Koji Ishii <kojii@chromium.org>
2025-05-17 20:44:02 +0900

    Use `setuptools-scm` (#184)

--- src/unicodedata_reader/__init__.py
+++ src/unicodedata_reader/__init__.py
@@ -1,3 +1,8 @@
+try:
+    from ._version import version as __version__  # type: ignore
+except ImportError:
+    __version__ = "0.0.0+unknown"
+
 from .entry import *
 from .reader import *
 from .compressor import *
